385c1e54cc19a72f9eef06fec80ce11208de75ac
Marek Kodr <kodrmare@fit.cvut.cz>
2017-09-19 10:49:17 +0200

    Update sendScript to server in sandbox

--- languages/jetbrains.mps.samples.VoiceMenu/sandbox/source_gen/jetbrains/mps/samples/VoiceMenu/send.sh
+++ languages/jetbrains.mps.samples.VoiceMenu/sandbox/source_gen/jetbrains/mps/samples/VoiceMenu/send.sh
@@ -4,7 +4,7 @@ mkdir sandbox
 cp sipJet_Mobile.conf sandbox/sip.conf
 cp extensionsJet_Mobile.conf sandbox/extensions.conf
 
-scp -r sandbox user@172.31.1.19:/home/user
+sshpass -p 12345678 scp -r sandbox user@172.31.1.19:/home/user
 
 rm sandbox/sip.conf
 rm sandbox/extensions.conf
